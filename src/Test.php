15a5bf83fd5f64cbf2ff5f48dcd8684376f98f49
wangziwen <317689136@qq.com>
2022-08-10 22:39:32 +0800

    feat:init

---
+++ src/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Wangziwen\MyComposer;
+
+class Test
+{
+    public function test()
+    {
+        echo <<<EOF
+            危乎高哉！
+            蜀道之难，难于上青天
+            尔来四万八千岁
+            不与秦塞通人烟
+EOF;
+
+    }
+}
